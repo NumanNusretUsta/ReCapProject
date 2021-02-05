0e575b62341ae1ffec7335dffec61d5b4a760d15
Numan Nusret Usta <numanusta75@gmail.com>
2021-02-05 23:38:23 +0300

    8.hafta ödev1

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,11 +1,11 @@
 ﻿using Business.Concrete;
-<<<<<<< HEAD
+
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
-=======
+
 using DataAccess.Concrete.InMemory;
->>>>>>> 9b58c89d37271569152581b9294c61dafe288a5a
+
 using System;
 
 namespace ConsoleUI
@@ -14,20 +14,21 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
+
             CarManager carManager = new CarManager(new EfCarDal());
 
-            carManager.Add(new Car {Id=2,BrandId=1,ColorId=1,ModelYear=2002,DailyPrice=10000,Description="Dizel" });
+            carManager.Add(new Car { Id = 2, BrandId = 1, ColorId = 1, ModelYear = 2002, DailyPrice = 10000, Description = "Dizel" });
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Description);
-=======
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
 
-            foreach (var product in productManager.GetAll())
-            {
-                Console.WriteLine(product.Description);
->>>>>>> 9b58c89d37271569152581b9294c61dafe288a5a
+                ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+                foreach (var product in productManager.GetAll())
+                {
+                    Console.WriteLine(product.Description);
+
+                }
             }
         }
     }
